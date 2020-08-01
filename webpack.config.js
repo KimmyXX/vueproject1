7d04851af9b028856655a94d483bd3d1c2eb9958
倪铖涛 <2922093951@qq.com>
2020-08-01 10:05:08 +0800

    配置了postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
+var postcss =require('postcss');
 
 module.exports = {
   mode: "development",
@@ -16,7 +17,20 @@ module.exports = {
   ],
   module: {
     rules: [
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader', 
+            options: {
+              importLoaders: 2, //如果sass文件里还引入了另外一个sass文件，另一个文件还会从最后一个loader向上解析。如果不加，就直接从css-loader开始解析。// 0 => no loaders (default); 1 => postcss-loader; 2 => postcss-loader, sass-loader
+              modules: true//开启css的模块打包。css样式不会和其他模块发生耦合和冲突
+            }
+          },
+          'postcss-loader'
+        ]
+      },
       { test: /\.(png|jpg|gif)$/, use: "file-loader" },
       // 加载字体文件
       {
@@ -24,7 +38,21 @@ module.exports = {
         use: { loader: "file-loader", options: { outputPath: "../fonts" } }
       },
       { test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"] },
-      // { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 
+          {
+            loader: 'css-loader', 
+            options: {
+              importLoaders: 2, //如果sass文件里还引入了另外一个sass文件，另一个文件还会从最后一个loader向上解析。如果不加，就直接从css-loader开始解析。// 0 => no loaders (default); 1 => postcss-loader; 2 => postcss-loader, sass-loader
+              modules: true//开启css的模块打包。css样式不会和其他模块发生耦合和冲突
+            }
+          },
+          'postcss-loader',//配置在css-loader后，在sass|less|style-loader 之前
+          'sass-loader'
+        ]
+      },
       {
         test: /\.vue$/,
         loader: "vue-loader",
